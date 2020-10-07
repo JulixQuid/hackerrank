b5a3321b35567289ee99cc5518b1d711eb43ca12
JulixQuid <30515622+JulixQuid@users.noreply.github.com>
2020-10-06 20:27:20 -0500

    added jumping on the clouds warm up challenges

--- interview_preparation_kit/warm_up_challenges.py
+++ interview_preparation_kit/warm_up_challenges.py
@@ -1,5 +1,29 @@
 # warm up challenges
 
+def sockMerchant(n, ar):
+    """
+        It must return an integer representing the number of matching pairs of
+        socks that are available.
+
+    Args:
+        n: int
+            the number of socks in the pile
+        ar: list
+            the colors of each sock
+
+    Returns:
+        pairs: int
+            the number of matching pairs
+    """
+    pair_control = dict()
+    for sock in ar:
+        if sock in pair_control.keys():
+            pair_control[sock] += 0.5
+        else:
+            pair_control[sock] = 0.5
+    pairs = sum([int(total) for total in pair_control.values()])
+    return pairs
+
 
 def countingValleys(steps, path):
     """
@@ -32,31 +56,26 @@ def countingValleys(steps, path):
     return valleys
 
 
-
-
-
-
-
-def sockMerchant(n, ar):
+def jumpingOnClouds(c):
     """
-        It must return an integer representing the number of matching pairs of
-        socks that are available.
 
     Args:
-        n: int
-            the number of socks in the pile
-        ar: list
-            the colors of each sock
+        c: list
+            an array of binary integers
+
 
     Returns:
-        pairs: int
-            the number of matching pairs
+
     """
-    pair_control = dict()
-    for sock in ar:
-        if sock in pair_control.keys():
-            pair_control[sock] += 0.5
-        else:
-            pair_control[sock] = 0.5
-    pairs = sum([int(total) for total in pair_control.values()])
-    return pairs
+    i = 0
+    steps = 0
+    while i < len(c):
+        print(i)
+        steps += 1
+        if i >= len(c)-3:
+            break
+        if c[i+2] == 0:
+            i += 1
+        i += 1
+
+    return steps
